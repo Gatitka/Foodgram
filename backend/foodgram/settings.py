d76a48e3adb05328d74fe61cc9892f24aef16357
Nataly Kirillova <gatitka@yandex.ru>
2023-06-05 12:23:50 +0200

    recipe model validators added

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-load_dotenv(os.path.join(os.path.dirname(BASE_DIR), 'infra/env for local PSG/.env'), verbose=True) # FOR LOCAL USAGE l
+load_dotenv(os.path.join(os.path.dirname(BASE_DIR), 'infra/env for local PSG/.env'), verbose=True)   # FOR LOCAL USAGE l
 # load_dotenv(os.path.join(os.path.dirname(BASE_DIR), '.env'), verbose=True)   # FOR SERVER USAGE
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
